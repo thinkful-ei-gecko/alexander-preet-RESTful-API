6945a7c0972a82e9925efc809e368dbe48cd5312
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-23 14:57:50 -0500

    section 7 done

--- scripts/index.js
+++ scripts/index.js
@@ -6,14 +6,6 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
 
-  api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    const item = items[0];
-    return api.updateItem(item.id, { name: 'foobar' });
-  })
-  .then(res => res.json())
-  .then(() => console.log('updated!'));
 });
 
 store.items.push(Item.create('apples'));
