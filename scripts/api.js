77f6736df7c048967fa6de352f6f28787ee1f054
Alexander Fukui <alexander.fukui@gmail.com>
2019-08-23 13:46:44 -0500

    section 5 done

--- scripts/api.js
+++ scripts/api.js
@@ -11,7 +11,11 @@ const api = (function(){
   
   function createItem(name) {
     const newItem = JSON.stringify({name});
-    return fetch(`${BASE_URL}/items`, { method: 'POST', headers: {'Content-Type':'application/json'}, body: newItem });
+    return fetch(`${BASE_URL}/items`, {
+      method: 'POST',
+      headers: {'Content-Type':'application/json'},
+      body: newItem
+    });
   }
 
   return {
